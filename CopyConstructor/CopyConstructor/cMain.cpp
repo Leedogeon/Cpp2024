c00377e8c1423a5569f79d15baddcf654eb325e4
Leedogeon <jszs98@naver.com>
2024-03-25 18:27:11 +0900

    &
    &

--- CopyConstructor/CopyConstructor/cMain.cpp
+++ CopyConstructor/CopyConstructor/cMain.cpp
@@ -8,6 +8,8 @@ public:
 	int num;
 public:
 	Obj() { cout << "생성자" << endl; }
+	Obj(int num) { cout << num << " 생성자" << endl; }
+	//Obj& other = a;
 	Obj(Obj& other) { cout << "복사 생성자" << endl; }
 	~Obj() { cout << "소멸자" << endl; }
 };
@@ -15,13 +17,17 @@ public:
 
 int main()
 {
+
+	Obj obj(1);
+
 	Obj a;	 //생성자
 	cout << "==============" << endl;
 	a.num = 1;
 
 	Obj b = a;	//복사 생성자
+	Obj c(a);   //복사 생성자
 
-	cout << b.num << endl;
+	cout << &b << endl;
 
 
 	return 0;  //소멸자, 소멸자
