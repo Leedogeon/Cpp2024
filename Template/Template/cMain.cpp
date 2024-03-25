373789056b0ef2246119e784f77da3e50f31bc3d
Leedogeon <jszs98@naver.com>
2024-03-25 19:33:25 +0900

    템플릿
    템플릿

---
+++ Template/Template/cMain.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+template<typename T>
+T Add(T a, T b)
+{
+	return a + b;
+}
+
+int main()
+{
+	cout << Add(1, 2) << endl;
+	cout << Add(1.1f, 2.2f) << endl;
+
+	return 0;
+}
