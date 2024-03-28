f8d4ef026743f32047d6b366dac1e2f6602ae52b
Leedogeon <jszs98@naver.com>
2024-03-28 18:24:37 +0900

    operator 사용
    operator 사용

--- Operator/Operator/cMain.cpp
+++ Operator/Operator/cMain.cpp
@@ -6,6 +6,15 @@ class Obj
 public:
 	int x;
 	int y;
+public:
+	Obj operator +(const Obj& other)
+	{
+		Obj obj;
+		obj.x = x + other.x;
+		obj.y = x + other.y;
+
+		return obj;
+	}
 };
 
 int main()
@@ -17,5 +26,8 @@ int main()
 	Obj b;
 	b.x = 2;
 	b.y = 2;
+
+	Obj c{ a.x + b.x, a.y + b.y };
+	Obj d = a + b;
 
 }
