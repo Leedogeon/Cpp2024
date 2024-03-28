52f54dc609322be58ad44358e32df598e5421179
Leedogeon <jszs98@naver.com>
2024-03-28 19:46:56 +0900

    변경금지 강조
    변경금지 강조

--- Const/Const/cMain.cpp
+++ Const/Const/cMain.cpp
@@ -4,16 +4,12 @@ using namespace std;
 
 class A
 {
-public:
-	//멤버 변수들
+private:
 	int a;
 	int b;
 public:
-	//이함수는 클라스의 멤버 변수 변경 불가
-	void Function()	const
-	{
-		//  a=12  << X
-	}
+	int GetA() const { return a; }
+
 };
 
 
