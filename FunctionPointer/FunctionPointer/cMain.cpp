5a803b26b80157ce655c636182988d87d40f57fe
Leedogeon <jszs98@naver.com>
2024-03-29 19:21:05 +0900

    using
    using

--- FunctionPointer/FunctionPointer/cMain.cpp
+++ FunctionPointer/FunctionPointer/cMain.cpp
@@ -2,20 +2,20 @@
 
 using namespace std;
 
+//typedef 함수반환형(*함수포인터 자료형 이름)(함수 매개변수 데이터 타입들);
+//typedef int(*Func)(int, int);
+
 int Add(int a, int b) { return a + b; }
 
-//typedef 함수반환형(*함수포인터 자료형 이름)(함수 매개변수 데이터 타입들);
-typedef int(*A)(int, int);
+//C++
+//using 타입정의 = 정의;
+using Func = int(*)(int, int);
 
 int main()
 {
-	int(*p)(int, int) = Add;
-	int(*p2)(int, int) = Add;
-
-	A p3 = Add;
-	A p4 = NULL;
-	p4 = p3;
-	cout << p4(1, 2) << endl;
+	Func a = NULL;
+	a = Add;
+	cout << a(1, 2) << endl;
 
 	return 0;
 }
