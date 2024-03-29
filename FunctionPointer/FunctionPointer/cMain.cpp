0fbee1986b0fd6e34cb07b7581ddac7524722146
Leedogeon <jszs98@naver.com>
2024-03-29 19:17:35 +0900

    typedef
    typedef

--- FunctionPointer/FunctionPointer/cMain.cpp
+++ FunctionPointer/FunctionPointer/cMain.cpp
@@ -2,28 +2,20 @@
 
 using namespace std;
 
-template<typename T>
-T Add(T a, T b) { return a + b; }
-template<typename T>
-T Sub(T a, T b) { return a - b; }
-template<typename T>
-T Mul(T a, T b) { return a * b; }
-template<typename T>
-T Div(T a, T b) { return a / b; }
+int Add(int a, int b) { return a + b; }
 
-template<typename T>
-T Calculator(T(*p)(T, T), T valueA, T valueB)
-{
-	return p(valueA, valueB);
-}
-
+//typedef 함수반환형(*함수포인터 자료형 이름)(함수 매개변수 데이터 타입들);
+typedef int(*A)(int, int);
 
 int main()
 {
-	cout << Calculator(Add, 1.1, 2.2) << endl;
-	cout << Calculator(Sub, 1, 2) << endl;
-	cout << Calculator(Mul, 1.1, 2.2) << endl;
-	cout << Calculator(Div, 1, 2) << endl;
+	int(*p)(int, int) = Add;
+	int(*p2)(int, int) = Add;
+
+	A p3 = Add;
+	A p4 = NULL;
+	p4 = p3;
+	cout << p4(1, 2) << endl;
 
 	return 0;
 }
