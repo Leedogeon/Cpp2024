f84cc756493458a77572e770c7d1748b53a1f18e
Leedogeon <jszs98@naver.com>
2024-04-01 19:07:02 +0900

    람다 + 함수포인터
    람다 + 함수포인터

--- LambdaFunction/LambdaFunction/cMain.cpp
+++ LambdaFunction/LambdaFunction/cMain.cpp
@@ -1,37 +1,15 @@
 #include <iostream>
 using namespace std;
 
+using P = void(*)();
+void A()
+{
+	cout << "Test" << endl;
+}
+
 int main()
 {
-	int numA = 1;
-	int numB = 2;
-	int numC = 3;
-	int numD = 4;
-	cout <<  "numA : " << &numA << endl;
-	cout << "numB : " << &numB << endl;
-	cout << "numC : " << &numC << endl;
-	cout << "numD : " << &numD << endl;
-	//새로 할당된 numA와 numB 는 람다에 속함
-	//int numA = numA(1); 복사해서 값을 넣어줌
-	//[&numA, numB]() {cout << "lambda numA :"  << &numA << endl; }();
-	[&]() {
-		cout << "lambda numA :" << &numA << endl;
-	cout << "lambda numB :" << &numB << endl;
-		}();
-
-	[=]() {
-		cout << "lambda numA :" << &numA << endl;
-	cout << "lambda numB :" << &numB << endl;
-		}();
-
-
-	[&,numA,numB]() {
-		cout << "lambda numA :" << &numA << endl;
-	cout << "lambda numB :" << &numB << endl;
-	cout << "lambda numC :" << &numC << endl;
-	cout << "lambda numD :" << &numD << endl;
-		}();
-
-	
-
+	P p;
+	p = []() {cout << "TEST" << endl; };
+	p();
 }
