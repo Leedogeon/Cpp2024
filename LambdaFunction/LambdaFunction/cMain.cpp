a333e94ee03ed54e898105184b1cd46f4128d5b3
Leedogeon <jszs98@naver.com>
2024-04-01 18:41:25 +0900

    []안 분류
    []안 분류

--- LambdaFunction/LambdaFunction/cMain.cpp
+++ LambdaFunction/LambdaFunction/cMain.cpp
@@ -5,18 +5,33 @@ int main()
 {
 	int numA = 1;
 	int numB = 2;
+	int numC = 3;
+	int numD = 4;
 	cout <<  "numA : " << &numA << endl;
 	cout << "numB : " << &numB << endl;
-
+	cout << "numC : " << &numC << endl;
+	cout << "numD : " << &numD << endl;
 	//새로 할당된 numA와 numB 는 람다에 속함
 	//int numA = numA(1); 복사해서 값을 넣어줌
 	//[&numA, numB]() {cout << "lambda numA :"  << &numA << endl; }();
-	[&]() {cout << "lambda numA :" << &numA << endl;
+	[&]() {
+		cout << "lambda numA :" << &numA << endl;
 	cout << "lambda numB :" << &numB << endl;
 		}();
 
-	[=]() {cout << "lambda numA :" << &numA << endl;
+	[=]() {
+		cout << "lambda numA :" << &numA << endl;
 	cout << "lambda numB :" << &numB << endl;
 		}();
+
+
+	[&,numA,numB]() {
+		cout << "lambda numA :" << &numA << endl;
+	cout << "lambda numB :" << &numB << endl;
+	cout << "lambda numC :" << &numC << endl;
+	cout << "lambda numD :" << &numD << endl;
+		}();
+
+	
 
 }
