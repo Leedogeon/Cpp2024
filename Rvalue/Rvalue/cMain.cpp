435692c0b18957be70c9c0d453158b7b605cf722
Leedogeon <jszs98@naver.com>
2024-04-02 19:43:27 +0900

    forward 2
    forward 2

--- Rvalue/Rvalue/cMain.cpp
+++ Rvalue/Rvalue/cMain.cpp
@@ -2,33 +2,13 @@
 
 using namespace std;
 
-//왼값 참조
-void Value(int& num)
-{
-	cout << "왼값 참조 : " << num << endl;
-}
-
-//오른값 참조
-void Value(int&& num)
-{
-	cout << "오른값 참조 : " << num << endl;
-}
-
-//보편 참조
-//T&& T 왼값 -> 왼값 / T 오른값 -> 오른값
-template<typename T> // template인데 &&
-void Forwarder(T&& arg) // 오른값 참조 아님
-{
-	Value(std::forward<T>(arg)); //인자의 원래 값 카테고리를 유지하며 전달
-}
 
 int main()
 {
 	int a = 5;
-
-	Forwarder(a); // a는 lvalue, T&& left value 대응
-
-	Forwarder(5); // 5는 rvalue, T&& right value 대응
+	std::move(a); // 왼값 --> 오른값으로 처리
+	std::forward<int>(a); //왼값 --> 왼값으로 처리
+	std::forward<int>(5); //오른값 --> 오른값으로 처리
 
 	return 0;
 }
