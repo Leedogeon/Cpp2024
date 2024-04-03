3680bd6be3d50f893bd158b0113f2909cdd31260
Leedogeon <jszs98@naver.com>
2024-04-03 18:28:25 +0900

    thread index
    thread index

--- Thread/Thread/cMain.cpp
+++ Thread/Thread/cMain.cpp
@@ -2,16 +2,19 @@
 #include<thread>
 using namespace std;
 
-void ThreadFunc()
+void ThreadFunc(int index)
 {
-	cout << "tread" << endl;
+	cout << "tread " << index << endl;
 }
 
 int main()
 {
-	thread t(ThreadFunc);
+	thread t(ThreadFunc, 1);
+	thread t2;
+	t2 = thread(ThreadFunc, 2);
 	cout << "main" << endl;
 
 	t.join();//thread Á¾·á
+	t2.join();
 	return 0;
 }
