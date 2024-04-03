80fe90441c45348885722dcbef9c03a119251d6f
Leedogeon <jszs98@naver.com>
2024-04-03 19:08:26 +0900

    atomic용 명령어
    atomic용 명령어

--- Thread/Thread/cMain.cpp
+++ Thread/Thread/cMain.cpp
@@ -4,24 +4,30 @@
 using namespace std;
 
 //res에 대한 동시접근이 원자적으로 차단
-atomic<int> res = 0;
+atomic<int> res;
 
 void Add()
 {
 	for (int i = 0; i < 100000; i++)
 	{
-		res++;
+		//res++;
+		res.fetch_add(1);
 	}
 }
 void Sub()
 {
 	for (int i = 0; i < 100000; i++)
 	{
-		res--;
+		//res--;
+		//res.fetch_add(-1);
+		res.fetch_sub(1);
 	}
 }
 int main()
 {
+	//res = 0;
+	res.store(0);
+
 	thread t1(Add);
 	thread t2(Sub);
 
