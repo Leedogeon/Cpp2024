31302d3ba0418fc1288385b1f4b59236e2a02495
Leedogeon <jszs98@naver.com>
2024-04-03 19:38:31 +0900

    Disconnect
    Disconnect

--- Thread/Thread/cMain.cpp
+++ Thread/Thread/cMain.cpp
@@ -4,21 +4,25 @@
 
 using namespace std;
 
-atomic<int> num = 0;
+atomic<bool> isConnected;
+
+void Disconnect()
+{
+	if (isConnected.exchange(false) == false)
+	{
+		return;
+	}
+	cout << "DisConnected" << endl;
+
+}
 
 int main()
 {
-	int expected = 0; // 예상되는 값
-	int desired = 1;  // 원하는값
+	isConnected.store(true);
 
-	//조건이 충족될 때까지 반복
-	bool result = num.compare_exchange_strong(expected, desired);
+	Disconnect();
+	Disconnect();
 
-	while (true)
-	{
-		//compare_exchange_weak 함수를 원자적으로 계속 값을 변경 할려고 시도
-		num.compare_exchange_weak(expected, desired);
-	}
 
 	return 0;
 }
