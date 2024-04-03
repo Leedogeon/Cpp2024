29a207322745b273dbd04e0f658c24d1d30ac91e
Leedogeon <jszs98@naver.com>
2024-04-03 19:28:59 +0900

    compare_exchange_weak 동작방식
    compare_exchange_weak 동작방식

--- Thread/Thread/cMain.cpp
+++ Thread/Thread/cMain.cpp
@@ -1,24 +1,37 @@
 #include <iostream>
-#include <thread>	
-#include <atomic>
+#include <thread>
+#include <atomic> 
+
 using namespace std;
 
 atomic<int> num = 0;
 
 int main()
 {
-	int expected = 0; //예상되는 값
-	int desired = 1; //원하는 값
+	int expected = 0; // 예상되는 값
+	int desired = 1;  // 원하는값
 
-	//num == expected -> num = desired;
+	//num(0) == expected(0) -> num = desired;
 	//값이 같아서 바꿧으면 true 아니면 false
-	bool res = num.compare_exchange_weak(expected,desired);
-	cout << res << endl;
-	//res = num.compare_exchange_weak(expected, desired);
-	//cout << res << endl;
+	bool result = num.compare_exchange_weak(expected, desired);
+	cout << result << endl;
 
-	//값이 바뀔때까지 반복
-	res =num.compare_exchange_strong(desired,expected);
-	cout << res << endl;
+	//num.compare_exchage_weak
+	if (num == expected) // num이 예상 되는 값이라면
+	{
+		//만약에 다른 스레드의 방해로 중간에 실패
+		num = desired;
+		expected = num;
+		return true;
+	}
+	else
+	{
+		expected = num;
+		return false;
+	}
+
+	//예상되는 값이 바뀜
+	cout << expected << endl;
+
 	return 0;
 }
