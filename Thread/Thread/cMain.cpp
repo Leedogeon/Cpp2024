2c7c8daae3302e97d0f1c4d572a09540cdd96849
Leedogeon <jszs98@naver.com>
2024-04-03 19:44:45 +0900

    mutex
    mutex

--- Thread/Thread/cMain.cpp
+++ Thread/Thread/cMain.cpp
@@ -1,28 +1,31 @@
 #include <iostream>
 #include <thread>
-#include <atomic> 
-
+#include <vector>
+#include <mutex>
 using namespace std;
 
-atomic<bool> isConnected;
+vector<int> numbers;;
+mutex mtx;
 
-void Disconnect()
+void Push()
 {
-	if (isConnected.exchange(false) == false)
+	for (int i = 0; i < 10000; i++)
 	{
-		return;
+		mtx.lock();
+		// 내 스레드가 건드리고 있으면 스레드가 못건드림
+		numbers.push_back(1);
+		mtx.unlock();
 	}
-	cout << "DisConnected" << endl;
-
 }
 
 int main()
 {
-	isConnected.store(true);
+	thread t1(Push);
+	thread t2(Push);
 
-	Disconnect();
-	Disconnect();
+	t1.join();
+	t2.join();
 
-
+	cout << numbers.size() << endl;
 	return 0;
 }
