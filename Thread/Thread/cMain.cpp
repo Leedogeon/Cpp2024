9228b1e7d0b14b62021cd93dfab6bc1433525d40
Leedogeon <jszs98@naver.com>
2024-04-03 19:13:16 +0900

    is_lock_free
    is_lock_free

--- Thread/Thread/cMain.cpp
+++ Thread/Thread/cMain.cpp
@@ -3,38 +3,20 @@
 #include <atomic>
 using namespace std;
 
-//res에 대한 동시접근이 원자적으로 차단
-atomic<int> res;
+struct MyStruct
+{
+	char a;
+	char b;
+	char c;
+	char d;
+};
 
-void Add()
-{
-	for (int i = 0; i < 100000; i++)
-	{
-		//res++;
-		res.fetch_add(1);
-	}
-}
-void Sub()
-{
-	for (int i = 0; i < 100000; i++)
-	{
-		//res--;
-		//res.fetch_add(-1);
-		res.fetch_sub(1);
-	}
-}
+atomic<MyStruct> myStruct;
+
+
 int main()
 {
-	//res = 0;
-	res.store(0);
-
-	thread t1(Add);
-	thread t2(Sub);
-
-	t1.join();
-	t2.join();
-
-	cout << res << endl;
+	cout << "lock free? : " << myStruct.is_lock_free()<< endl;
 
 	return 0;
 }
