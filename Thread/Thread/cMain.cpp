07f517838a9c3c3575b5c5a73a75be1b45582294
Leedogeon <jszs98@naver.com>
2024-04-04 18:19:16 +0900

    lock_guard
    lock_guard

--- Thread/Thread/cMain.cpp
+++ Thread/Thread/cMain.cpp
@@ -7,14 +7,33 @@ using namespace std;
 vector<int> numbers;;
 mutex mtx;
 
+//lockguard 구현
+template<typename T>
+class LockGuard
+{
+private:
+	T* mutx;
+public:
+	LockGuard(T& m)
+	{
+		mutx = &m;
+		mutx->lock();
+	}
+	~LockGuard()
+	{
+		mutx->unlock();
+	}
+};
+
 void Push()
 {
 	for (int i = 0; i < 10000; i++)
 	{
-		mtx.lock();
+		//mtx.lock();
+		//LockGuard<mutex> lock(mtx);
+		lock_guard<mutex> lock(mtx);
 		// 내 스레드가 건드리고 있으면 스레드가 못건드림
 		numbers.push_back(1);
-		mtx.unlock();
 	}
 }
 
