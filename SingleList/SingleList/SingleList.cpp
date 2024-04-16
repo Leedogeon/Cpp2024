97bf33c052fd9d1882546250e4a5c8e748750ad0
Leedogeon <jszs98@naver.com>
2024-04-16 19:43:31 +0900

    미완
    미완

--- SingleList/SingleList/SingleList.cpp
+++ SingleList/SingleList/SingleList.cpp
@@ -14,18 +14,85 @@ void SingleList::AddNode(int _data)
 		curNode->next = nNode;
 		curNode = curNode->next;
 	}
+	count++;
 }
 
 void SingleList::InsertNode(int _index, int _data)
 {
+	if (_index > count) return;
+
+	Node* nNode = new Node;
+	nNode->data = _data;
+	Node* Prev = new Node;
+	Prev = head;
+	for (int i = 0; i < _index; i++)
+	{
+		Prev = Prev->next;
+	}
+	nNode->next = Prev->next;
+	Prev->next = nNode;
+
+
+	if (nNode->next == nullptr)
+	{
+		curNode = nNode;
+	}
+	count++;
 }
 
 void SingleList::UpdateNode(int _index, int _data)
 {
+	if (_index > count) return;
+
+	Node* nNode = new Node;
+	nNode->data = _data;
+	Node* Prev = new Node;
+	Prev = head;
+	for (int i = 0; i < _index; i++)
+	{
+		Prev = Prev->next;
+	}
+	Prev->data = nNode->data;
+
 }
 
 void SingleList::DeleteNodeData(int _data)
 {
+	Node* nNode = new Node;
+	nNode = head;
+	for (int i = 0; i < count; i++)
+	{
+		if (nNode->data = _data)
+		{
+			if (nNode == head)
+			{
+				/*if (head->next == nullptr)
+				{
+					delete nNode;
+				}*/
+				head = head->next;
+			}
+			else
+			{
+				Node* Next = new Node;
+				Next = nNode->next;
+				if (Next->next == nullptr)
+				{
+					nNode->next = nullptr;
+				}
+				else
+				{
+					
+					nNode->next = Next;
+				}
+				
+				break;
+			}
+			
+		}
+		else nNode = nNode->next;
+	}
+
 }
 
 void SingleList::DeleteIndex(int _index)
--- SingleList/SingleList/cMain.cpp
+++ SingleList/SingleList/cMain.cpp
@@ -3,5 +3,17 @@ int main()
 {
 	SingleList sin;
 	sin.AddNode(10);
+	sin.AddNode(20);
+	sin.AddNode(30);
+	sin.AddNode(40);
+	sin.InsertNode(0, 50);
+	
+	//cout << sin.head->data << endl;
+	//cout << sin.head->next->data << endl;
+	//cout << sin.head->next->next->data << endl;
+	//cout << sin.head->next->next->next->data << endl;
+	//cout << sin.head->next->next->next->next->data << endl;
+
+	sin.DeleteNodeData(10);
 
 }
