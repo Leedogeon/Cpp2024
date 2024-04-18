8cbe5bdd34a1abd6ab7391bea405780ef0f20cfc
Leedogeon <jszs98@naver.com>
2024-04-18 19:47:36 +0900

    inser 미완성
    inser 미완성

---
+++ DoubleList/DoubleList/cMain.cpp
@@ -0,0 +1,22 @@
+#include "DoubleList.h"
+
+int main()
+{
+	DoubleList DL;
+
+	DL.Push_Back(10);
+	DL.Push_Back(20);
+	DL.Push_Back(30);
+	DL.Push_Back(40);
+	DL.Push_Back(50);
+	DL.Push_Back(60);
+	DL.Insert(0, 3, 10);
+	cout << "count = " << DL.count << endl;
+	DL.PrintAll();
+	DL.Clear();
+	cout << "count = " << DL.count << endl;
+	cout << "ALL" << endl;
+	DL.PrintAll();
+
+	return 0;
+}
