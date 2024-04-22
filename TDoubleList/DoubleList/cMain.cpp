8c71c46e582b4547e9abf8dd9bac16ca51765166
Leedogeon <jszs98@naver.com>
2024-04-22 13:12:06 +0900

    테스트
    테스트

--- TDoubleList/DoubleList/cMain.cpp
+++ TDoubleList/DoubleList/cMain.cpp
@@ -2,7 +2,7 @@
 int main()
 {
 	DoubleList<int> DL;
-
+	DoubleList<char> cDL;
 	DL.Push_Back(10);
 	DL.Push_Back(20);
 	DL.Push_Back(30);
@@ -13,6 +13,14 @@ int main()
 	DL.Insert(1, 2, 56);
 
 	DL.PrintAll();
+
+
+	cDL.Push_Back('a');
+	cDL.Push_Back('b');
+	cDL.Push_Back('c');
+	cDL.Push_Back('d');
+	cDL.Push_Back('e');
+	cDL.PrintAll();
 
 	return 0;
 }
