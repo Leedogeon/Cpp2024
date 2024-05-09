25abd25b3189a973fe119a250207e794156fbb7d
Leedogeon <jszs98@naver.com>
2024-05-09 18:27:15 +0900

    Bubble
    Bubble

---
+++ Bubble/Bubble/cMain.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+#define SWAP(a,b) {int temp; temp = a; a = b; b = temp;}
+
+void BubbleSort(int* pNum, int num);
+void Show(int* pArray, int num);
+
+void main()
+{
+	int array[10] = { 3,0,1,8,7,2,5,4,6,9 };
+	BubbleSort(array, 10);
+
+	
+}
+
+void BubbleSort(int* pArray, int num)
+{
+	for (int i = 0; i<10; i++)
+	{
+		for (int j = 0; j < 9; j++)
+		{
+			if (pArray[j] > pArray[j+1])
+			{
+				SWAP(pArray[j], pArray[j+1]);
+			}
+		}
+	}
+	Show(pArray, num);
+	//±¸Çö
+}
+
+void Show(int* pArray, int num)
+{
+	for (int i = 0; i < num; i++)
+	{
+		cout << pArray[i] << " ";
+	}
+
+	cout << endl;
+}
+
