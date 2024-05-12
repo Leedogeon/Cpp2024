79b95989130134ce216966f8e1c8e5790e89e15f
Leedogeon <jszs98@naver.com>
2024-05-13 00:17:29 +0900

    완성
    완성

--- Merge/Merge/cMain.cpp
+++ Merge/Merge/cMain.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
-#include <vector>
 using namespace std;
-
+int arr[10] = {};
 #define SWAP(a,b) {int temp; temp = a; a = b; b = temp;}
 
 void MergeSort(int* pNum, int left, int right);
@@ -11,94 +10,54 @@ void main()
 {
 	int array[10] = { 4,2,8,6,0,5,1,7,3,9 };
 	MergeSort(array, 0, 9);
-	
+	Show(array, 10);
 }
 
 void MergeSort(int* pNum, int left, int right)
 {
-	int half = (right + left) / 2;
-	int low = left;
-	int high = right;
+	if (left >= right) return;
 
-	cout << "low = " << low << endl;
-	cout << "high = " <<high << endl;
+	int half = (left + right) / 2;
+	int lt = left;
+	int rt = right;
+	int md = half + 1;
+	int i = left;
 
-	if (low < half)
-		MergeSort(pNum, low, half);
+	MergeSort(pNum, left, half);
+	MergeSort(pNum, half + 1, right);
 
-	if (half + 1 < high)
-		MergeSort(pNum, half + 1, high);
-
-	vector<int> temp;	
-
-	while (low < high)
+	while (lt <= half && md <= right)
 	{
-		if (pNum[low] > pNum[high])
+		if (pNum[lt] < pNum[md])
 		{
-			temp.push_back(pNum[high]);
-			high--;
+			arr[i++] = pNum[lt++];
 		}
 		else
 		{
-			temp.push_back(pNum[low]);
-			low++;
+			arr[i++] = pNum[md++];
 		}
 	}
-	for (int j = 0; j < temp.size(); j++)
+
+	if (lt > half)
 	{
-		pNum[left] = temp[j];
+		for (int k = md; k <= right; k++)
+		{
+			arr[i++] = pNum[k];
+		}
 	}
-	
+	else
+	{
+		for (int k = lt; k <= half; k++)
+		{
+			arr[i++] = pNum[k];
+		}
+	}
 
+	for (int k = left; k <= right; k++)
+	{
+		pNum[k] = arr[k];
+	}
 
-
-	/*//if (left == 0 && right == 9)
-	//{
-	//	int arr2[10] = {};
-
-	//	int i = 0;
-	//	int mid = right / 2 + 1;
-	//	while (true)
-	//	{
-	//		if (pNum[left] < pNum[mid])
-	//		{
-	//			arr2[i] = pNum[left];
-	//			left++;
-	//		}
-	//		else
-	//		{
-	//			arr2[i] = pNum[mid];
-	//			mid++;
-	//		}
-	//		i++;
-	//		if (left > half || mid > right) break;
-	//	}
-
-	//	//if (low > half)
-	//	//{
-	//	//	for (int k = mid; k <= high; k++)
-	//	//	{
-	//	//		arr2[i] = pNum[k];
-	//	//		i++;
-	//	//	}
-	//	//}
-	//	//else
-	//	//{
-	//	//	for (int k = low; k <= half; k++)
-	//	//	{
-	//	//		arr2[i] = pNum[k];
-	//	//		i++;
-	//	//	}
-	//	//}
-	//	cout << "arr2" << endl;
-	//	Show(arr2, 10);
-
-	//}*/
-
-
-
-	Show(pNum, 10);
-	//±¸Çö
 }
 
 void Show(int* pArray, int num)
